f248d2b21cfca77bfe124631a8f5405161d7eac9
MichaelCheers149 <michael.cheers@zzzzzzzz149.addy.io>
2024-11-08 04:12:34 +0900

    Update plan

--- config/products.js
+++ config/products.js
@@ -3,18 +3,18 @@ const products = [
     id: "prod_1",
     title: "Basic Plan",
     description: "Basic subscription plan",
-    price: "10.00",
+    price: "100.00",
     interval: "monthly",
     color: "#3f51b5",
   },
-  {
-    id: "prod_2",
-    title: "Pro Plan",
-    description: "Pro subscription plan",
-    price: "20.00",
-    interval: "monthly",
-    color: "#47eb2a",
-  },
+  // {
+  //   id: "prod_2",
+  //   title: "Pro Plan",
+  //   description: "Pro subscription plan",
+  //   price: "20.00",
+  //   interval: "monthly",
+  //   color: "#47eb2a",
+  // },
 ];
 
 module.exports = products;
