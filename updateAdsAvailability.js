f4680549754350eaa8e83543c1a1b9523f25d640
MichaelCheers149 <michael.cheers@zzzzzzzz149.addy.io>
2024-11-12 12:59:35 +0900

    Start after mongodb connection

--- updateAdsAvailability.js
+++ updateAdsAvailability.js
@@ -2,45 +2,48 @@ const Ads = require("./models/ads");
 const Company = require("./models/company");
 const { getAuthorizeSubscriptionStatus } = require("./config/authorize");
 
-require("./config/db")();
-
-setInterval(async () => {
-  try {
-    const ads = await Ads.find({});
-    console.log(
-      new Date().toLocaleString(),
-      `You have ${ads.length} ads totally`
-    );
-    let availableAds = 0;
-    for (const ad of ads) {
-      ad.isAvailable = false;
+require("./config/db")()
+  .then(() => {
+    setInterval(async () => {
       try {
-        const company = await Company.findById(ad.companyId);
-        if (
-          company &&
-          company.authorizeSubscriptionId &&
-          company.authorizeSubscriptionId !== ""
-        ) {
-          const { expired, isPending } = await getAuthorizeSubscriptionStatus({
-            subscriptionId: company.authorizeSubscriptionId,
-            includeTransactions: true,
-          });
+        const ads = await Ads.find({});
+        console.log(
+          new Date().toLocaleString(),
+          `You have ${ads.length} ads totally`
+        );
+        let availableAds = 0;
+        for (const ad of ads) {
+          ad.isAvailable = false;
+          try {
+            const company = await Company.findById(ad.companyId);
+            if (
+              company &&
+              company.authorizeSubscriptionId &&
+              company.authorizeSubscriptionId !== ""
+            ) {
+              const { expired, isPending } =
+                await getAuthorizeSubscriptionStatus({
+                  subscriptionId: company.authorizeSubscriptionId,
+                  includeTransactions: true,
+                });
 
-          if (!expired && !isPending) {
-            ad.isAvailable = true;
-            availableAds++;
-          }
+              if (!expired && !isPending) {
+                ad.isAvailable = true;
+                availableAds++;
+              }
+            }
+          } catch (error) {}
+
+          await ad.save();
+
+          console.log(
+            new Date().toLocaleString(),
+            `You have ${availableAds} available ads.`
+          );
         }
-      } catch (error) {}
-
-      await ad.save();
-
-      console.log(
-        new Date().toLocaleString(),
-        `You have ${availableAds} available ads.`
-      );
-    }
-  } catch (error) {
-    console.log(error);
-  }
-}, 10 * 60 * 1000);
+      } catch (error) {
+        console.log(error);
+      }
+    }, 10 * 60 * 1000);
+  })
+  .catch(() => {});
